f3ed579c839f1c59ff0a64fd7a49403713d2847b
EminaBegic <begicema@gmail.com>
2023-05-19 09:28:53 +0200

    promjene

--- src/Chat/Message.jsx
+++ src/Chat/Message.jsx
@@ -5,16 +5,16 @@ export default function Message({message, member}) {
     return(
             (message.member.id === member.id) ?
             (        
-            <li className=' Messages-message currentMember'>
-                <div className='Message-content'>
+            <li className=' Message currentMember'>
+                <div className='Message-text'>
                     <h2 className='text' style={{backgroundColor: message.member.clientData.color}}>{message.text}</h2>
                     <h4 className='username'>{message.member.clientData.username}</h4>
                 </div>
             </li>
             ) :
             (        
-            <li className=' Messages-message'>
-                <div className='Message-content'>
+            <li className=' Message'>
+                <div className='Message-text'>
                     <h2 className='text' style={{backgroundColor: message.member.clientData.color}}>{message.text}</h2>
                     <h4 className='username'>{message.member.clientData.username}</h4>
                                     </div>
